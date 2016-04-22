43c965d959bccb4d0fad7653f98386892ce81073
Josh Gao <jmgao@google.com>
2016-04-22 12:00:10 -0700

    Add hacky std::to_string overloads.

--- src/Utils.h
+++ src/Utils.h
@@ -6,6 +6,16 @@
 std::string getWorkingDir();
 std::vector<std::string> collectFiles(const char* directory);
 
+namespace std {
+  static __attribute__((unused)) std::string to_string(const char* c) {
+    return c;
+  }
+
+  static __attribute__((unused)) const std::string to_string(const std::string& str) {
+    return str;
+  }
+}
+
 template <typename Collection>
 static std::string Join(Collection c, const std::string& delimiter = ", ") {
   std::string result;
